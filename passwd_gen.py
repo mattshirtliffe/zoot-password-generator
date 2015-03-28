a7e23f3c34f8ebc597ff87c187a7e8f2e75a54b5
MatthewShirtliffe <matthew.shirtliffe@gmail.com>
2015-03-28 15:15:33 +0000

    first commit

---
+++ passwd_gen.py
@@ -0,0 +1,66 @@
+import string
+import random
+
+
+# TODO Remember with Phenetic alpha
+# TODO Create unittests
+# TODO write with pgp key
+
+class Password():
+    
+    length = 8
+    letters = True
+    mixedcase = True
+    numbers = False
+    punctuation = False
+
+    phenetic_alpha = {"A":"Alpha","B":"Bravo","C":"Charlie","D":"Delta","E":"Echo","F":"Foxtrot","G":"Golf","H":"Hotel","I":"India",
+                      "J":"Juliet","K":"Kilo","L":"Lima","M":"Mike","N":"November","O":"Oscar","P":"Papa","Q":"Quebec",
+                      "R":"Romeo","S":"Sierra","T":"Tango","U":"Uniform","V":"Victor","W":"Whiskey","X":"X-ray","Y":"Yankee",
+                      "Z":"Zulu"}
+
+    def __init__(self,length=8,letters=True,mixedcase=True,numbers=False,punctuation=False):
+        self.length = length
+        self.letters = letters
+        self.mixedcase = mixedcase
+        self.numbers = numbers
+        self.punctuation = punctuation 
+
+    def create_password(self,length=None,repeat=True):
+        
+        if not length:
+            length = self.length
+
+        char_selection=""
+        if self.letters == True:
+            if self.mixedcase == True:
+                char_selection += string.ascii_letters
+            else:
+                char_selection += string.ascii_lowercase
+
+        if self.numbers == True:
+            char_selection += string.digits
+        
+        if self.punctuation == True:
+            char_selection += string.punctuation
+    
+        
+        if not repeat:
+            key = ""
+            while(len(key)!=length):
+                # issue what is not possible?
+                # trying to produce a string with char we have if the len required
+                # exceeds the number of chars available
+                key += random.choice(char_selection)
+                key = "".join(set(key))
+        else:
+            key = ''
+            for i in range(length):
+                key += random.choice(char_selection)
+
+        return key
+
+if __name__ == "__main__":
+    p = Password(length=8,letters=True,mixedcase=True,numbers=False,punctuation=False)
+    print p.create_password()
+    print p.create_password(repeat=False)
